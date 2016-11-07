5360d363372640404f07e2e0d5a467f5220fea2c
Dream <service@sunfounder.com>
2016-11-07 01:37:18 +0000

    fix bugs

--- rpicar2/__init__.py
+++ rpicar2/__init__.py
@@ -1,34 +1,33 @@
 import sys
-import front_wheels
-from SunFounder.PCA9685 import Servo
+from rpicar2 import front_wheels
+from rpicar2.SunFounder_PCA9685 import Servo
 
 def main():
-    """Entry point for the application script"""
-    print("Call your main application code here")
-    if lenth(sys.argv) > 2:
-    	if sys.argv[1] == "servo-install":
-    		if lenth(sys.argv) > 3:
-    			usage()
-			Servo.install()
-    	elif sys.argv[1] == "front-wheel-test":
-    		if lenth(sys.argv) > 3:
-    			try:
-    				chn = int(sys.argv[2])
-    			except:
-    				usage()
-    			if 0 < chn < 16 :
-    				front_wheels.test(chn)
-    			else:
-    				usage()
-    		front_wheels.test()
-    	else:
-    		usage()
+    if len(sys.argv) >= 2:
+        if sys.argv[1] == "servo-install":
+            if len(sys.argv) >= 3:
+                usage()
+            Servo.install()
+        elif sys.argv[1] == "front-wheel-test":
+            if len(sys.argv) >= 3:
+                try:
+                    chn = int(sys.argv[2])
+                except:
+                    usage()
+                if 0 <= chn <= 15 :
+                    front_wheels.test(chn)
+                else:
+                    usage()
+            front_wheels.test()
+        else:
+            print sys.argv[1]
+            usage()
     else:
-    	usage()
+        usage()
 
 def usage():
-	print "Usage:  sunfounder-rpicar2 [Command] [value]"
-	print "Commands:"
-	print "    servo-install                Set 16 channel servos to 90 degree for installation"
-	print "    front-wheel-test [chn]       Test the front wheel steering servo connect to chn, chn default 0"
-	quit()
+    print "Usage:  sunfounder-rpicar2 [Command] [value]"
+    print "Commands:"
+    print "  servo-install              Set 16 channel servos to 90 degree for installation"
+    print "  front-wheel-test [chn]     Test the steering servo connect to chn, chn default 0"
+    quit()
