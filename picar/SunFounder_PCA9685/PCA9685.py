ed118d034f3f537f2d13b59cdb0cb8fabe09bb14
dsdsds <cavonxx@gmail.com>
2019-10-30 11:40:20 +0800

    fix No module name command

--- picar/SunFounder_PCA9685/PCA9685.py
+++ picar/SunFounder_PCA9685/PCA9685.py
@@ -38,93 +38,18 @@ class PWM(object):
     _INVRT              = 0x10
     _OUTDRV             = 0x04
 
-
-    RPI_REVISION_0 = ["900092"]
-    RPI_REVISION_1_MODULE_B  = ["Beta", "0002", "0003", "0004", "0005", "0006", "000d", "000e", "000f"]
-    RPI_REVISION_1_MODULE_A  = ["0007", "0008", "0009",]
-    RPI_REVISION_1_MODULE_BP = ["0010", "0013"]
-    RPI_REVISION_1_MODULE_AP = ["0012"]
-    RPI_REVISION_2_MODULE_B  = ["a01041", "a21041"]
-    RPI_REVISION_3_MODULE_B  = ["a02082", "a22082", "a32082"]
-    RPI_REVISION_3_MODULE_BP = ["a020d3"]
-    RPI_REVISION_3_MODULE_AP = ["9020e0"]
-
     _DEBUG = False
     _DEBUG_INFO = 'DEBUG "PCA9685.py":'
 
-    def _get_bus_number(self):
-        pi_revision = self._get_pi_revision()
-        if   pi_revision == '0':
-            return 0
-        elif pi_revision == '1 Module B':
-            return 0
-        elif pi_revision == '1 Module A':
-            return 0
-        elif pi_revision == '1 Module B+':
-            return 1
-        elif pi_revision == '1 Module A+':
-            return 0
-        elif pi_revision == '2 Module B':
-            return 1
-        elif pi_revision == '3 Module B':
-            return 1
-        elif pi_revision == '3 Module B+':
-            return 1
-        elif pi_revision == '3 Module A+':
-            return 1
-
-    def _get_pi_revision(self):
-        "Gets the version number of the Raspberry Pi board"
-        # Courtesy quick2wire-python-api
-        # https://github.com/quick2wire/quick2wire-python-api
-        # Updated revision info from: http://elinux.org/RPi_HardwareHistory#Board_Revision_History
-        try:
-            f = open('/proc/cpuinfo','r')
-            for line in f:
-                if line.startswith('Revision'):
-                    if line[11:-1] in self.RPI_REVISION_0:
-                        return '0'
-                    elif line[11:-1] in self.RPI_REVISION_1_MODULE_B:
-                        return '1 Module B'
-                    elif line[11:-1] in self.RPI_REVISION_1_MODULE_A:
-                        return '1 Module A'
-                    elif line[11:-1] in self.RPI_REVISION_1_MODULE_BP:
-                        return '1 Module B+'
-                    elif line[11:-1] in self.RPI_REVISION_1_MODULE_AP:
-                        return '1 Module A+'
-                    elif line[11:-1] in self.RPI_REVISION_2_MODULE_B:
-                        return '2 Module B'
-                    elif line[11:-1] in self.RPI_REVISION_3_MODULE_B:
-                        return '3 Module B'
-                    elif line[11:-1] in self.RPI_REVISION_3_MODULE_BP:
-                        return '3 Module B+'
-                    elif line[11:-1] in self.RPI_REVISION_3_MODULE_AP:
-                        return '3 Module A+'
-                    else:
-                        print("Error. Pi revision didn't recognize, module number: %s" % line[11:-1])
-                        print('Exiting...')
-                        quit()
-        except Exception as e:
-            f.close()
-            print(e)
-            print('Exiting...')
-            quit()
-        finally:
-            f.close()
-
-    def __init__(self, bus_number=None, address=0x40):
+    def __init__(self, bus_number=1, address=0x40):
         self.address = address
-        if bus_number == None:
-            self.bus_number = self._get_bus_number()
-        else:
-            self.bus_number = bus_number
+        self.bus_number = bus_number
         self.bus = smbus.SMBus(self.bus_number)
 
     def _debug_(self,message):
         if self._DEBUG:
             print(self._DEBUG_INFO,message)
     
-
     def setup(self):
         '''Init the class with bus_number and address'''
         self._debug_('Reseting PCA9685 MODE1 (without SLEEP) and MODE2')
@@ -158,22 +83,27 @@ class PWM(object):
             print(i)
             self._check_i2c()
 
+    def _run_command(self, cmd):
+        import subprocess
+        p = subprocess.Popen(
+            cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        result = p.stdout.read().decode('utf-8')
+        status = p.poll()
+        # print(result)
+        # print(status)
+        return status, result
+
     def _check_i2c(self):
-        import commands
-        bus_number = self._get_bus_number()
-        print("\nYour Pi Rivision is: %s" % self._get_pi_revision())
-        print("I2C bus number is: %s" % bus_number)
+        from os import listdir
+        print("I2C bus number is: %s" % self.bus_number)
         print("Checking I2C device:")
-        cmd = "ls /dev/i2c-%d" % bus_number
-        output = commands.getoutput(cmd)
-        print('Commands "%s" output:' % cmd)
-        print(output)
-        if '/dev/i2c-%d' % bus_number in output.split(' '):
-            print("I2C device setup OK")
+        devices = listdir("/dev/")
+        if "i2c-%d" % self.bus_number in devices:
+            print("I2C device exist.")
         else:
-            print("Seems like I2C have not been set, Use 'sudo raspi-config' to set I2C")
+            print("Seems like I2C have not been set, run 'sudo raspi-config' to enable I2C")
         cmd = "i2cdetect -y %s" % self.bus_number
-        output = commands.getoutput(cmd)
+        _, output = self._run_command(cmd)
         print("Your PCA9685 address is set to 0x%02X" % self.address)
         print("i2cdetect output:")
         print(output)
